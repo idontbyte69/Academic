ceaf48fccc32c2a36eba2cddaee46f23e61dceaa
idontbyte <tanveer.cse69@gmail.com>
2025-02-03 20:00:35 +0600

    a small change in the L3 Task

--- AI LAB CSE 316/CLP/CLP1/L3.py
+++ AI LAB CSE 316/CLP/CLP1/L3.py
@@ -4,5 +4,5 @@ for i in range (50, 100):
     if i%3 == 0 and i%5 != 0:
         sum += i        
 
-print(sum) 
+print("Sum of all numbers between 50 - 1000 which are divisible by 3 and not divisible by 5: ", sum) 
     
