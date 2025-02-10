9cd6d6d1ac7775490bf4a159efbf2f7edcfefc71
idontbyte <tanveer.cse69@gmail.com>
2025-02-10 23:29:13 +0600

    testing

---
+++ AI LAB CSE 316/CLP/CLP2/L2.py
@@ -0,0 +1,7 @@
+list1 = [1, 2, 3, 4, 5]
+list2 = [4, 5, 6, 7, 8]
+common_elements = []
+for item in list1:
+    if item in list2 and item not in common_elements:
+        common_elements.append(item)
+print("Common Elements:", common_elements)
